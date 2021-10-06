eb9d776c579ba0828648ce61b4edcea3b40e70e2
ignurof <ignurof@outlook.com>
2021-10-06 15:07:35 +0200

    setup configs based on official example and https://github.com/ignurof/example-svelte-express

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    port: 3000,
+
+    svelteViewEngine: {
+        env: "dev",
+        template: `${__dirname}/src/template.svelte`,
+        dir: `${__dirname}/src/routes`,
+        type: "svelte",
+        buildDir: `${__dirname}/build/routes`,
+        assetsPrefix: "/assets/",
+    },
+};
