7b0134bd3661cd6daedc532f966d49bfbaaa19af
ignurof <ignurof@outlook.com>
2021-10-06 15:12:45 +0200

    add example Index.svelte in /routes

---
+++ src/Routes/Index.svelte
@@ -0,0 +1,17 @@
+<script>
+    // Import payload handler (optional if using export)
+    import payload from "svelte-view-engine/payload";
+    // Example component (doesn't get rendered by the view engine outside of this file)
+    import Example from "../Components/Example.svelte";
+    // This is the same
+    export let a;
+    // As this
+    let { b } = payload.get();
+</script>
+
+<style>
+</style>
+
+<Example/>
+<p>Prop A is: {a}</p>
+<p>Prop B is: {b}</p>
