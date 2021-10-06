ea9afbc7d6ce45f7572c5737ed3389837846689d
ignurof <ignurof@outlook.com>
2021-10-06 15:11:13 +0200

    add recommended svelte-view-engine template

---
+++ src/template.svelte
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		${head}
+		<link rel="stylesheet" href="${cssPath}">
+	</head>
+	<body>
+		${html}
+		<script src="${jsPath}"></script>
+		<script>
+			props = ${props};
+			
+			new ${name}({
+				target: document.body,
+				hydrate: true,
+				props: props
+			});
+		</script>
+	</body>
+</html>
