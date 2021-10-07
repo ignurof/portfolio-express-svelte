e1f39681211db2604676923bb6c0b46c0fc82522
ignurof <ignurof@outlook.com>
2021-10-07 18:53:32 +0200

    update quotegenerator to use axios module instead

--- src/quotegenerator.js
+++ src/quotegenerator.js
@@ -1,9 +1,16 @@
-const request = require('request');
+const axios = require('axios');
 
 // Fetches all public scrapped quotes from Kanye using https://api.kanye.rest/ { quote: "text" }
 
 const GetQuote = () => {
-    
+    axios.get("https://api.kanye.rest/").then((response) => {
+        console.log(response);
+    }).catch((error) => {
+        console.error(error);
+    }).then(() => {
+        // The last .then always happens regardsless of outcome
+        console.log("Fetch completed or not, who knows");
+    });
 }
 
 module.exports = GetQuote;
