fac050c2af5ebb3431daae9caa64f705000d555a
ignurof <ignurof@outlook.com>
2021-10-07 15:48:50 +0200

    remove fetch api from quotegenerator as the fetch api does not exist in NodeJS (LUL)

--- src/quotegenerator.js
+++ src/quotegenerator.js
@@ -2,19 +2,7 @@
 
 // Asyncronous method constant
 const GetQuote = async() => {
-    // Make the fetch call and await the request response
-    let request = await fetch("https://api.kanye.rest");
-
-    // If fetch request gets OK status
-    if(request.ok){
-        let response = request.json();
-        console.log(response);
-        return response.quote;
-    }
-
-    // Successfull early returns means we should not end up here
-    console.error("Something went wrong with the coolQuote request");
-    return "error coolQuote";
+    
 }
 
 module.exports = GetQuote;
