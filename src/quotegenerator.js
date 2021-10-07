2184eb2d2cab03aaed7ed831ac3dd06982b02d33
ignurof <ignurof@outlook.com>
2021-10-07 15:54:26 +0200

    new quotegenerator using node-fetch

--- src/quotegenerator.js
+++ src/quotegenerator.js
@@ -1,8 +1,14 @@
+import { response } from "express";
+import fetch from "node-fetch";
+
 // Fetches all public scrapped quotes from Kanye using https://api.kanye.rest/ { quote: "text" }
 
 // Asyncronous method constant
 const GetQuote = async() => {
-    
+    let response = await fetch("https://api.kanye.rest/");
+    let data = await response.json();
+
+    console.log(data);
 }
 
 module.exports = GetQuote;
