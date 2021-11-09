6db598f9aac0f5ff6088652cc42a39300eac6b5f
ignurof <ignurof@outlook.com>
2021-11-09 17:13:58 +0100

    cleanup filehandler and make it work properly using promises (async/await) style

--- src/filehandler.js
+++ src/filehandler.js
@@ -1,48 +1,47 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-// This returns a servervar JSON object thats been read from file if the file existed on server startup
-const InitFile = (fileName) => {
-    fs.stat(fileName, (err) => {
-        if(err == null){
-            // File exist, read it and update server vars
-            ReadFile(fileName);
-        } else if(err.code === "ENOENT"){
-            // File does not exist, create one
-            let data = "filecontentsplaceholder";
-            fs.writeFile(fileName, data, (err) => {
-                if(err) return console.error("Create file error");
+const InitFile = async(fileName) => {
+    // Set the filename path
+    fileName = "admindb/" + fileName + ".json";
 
-                console.log("Created file" + fileName);
-            })
-        } else{
-            throw err;
-        }
-    });
+    let data = [{"test": "data"}];
+
+    // Try-Catch best practice for async (wait for code to complete, aka promises)
+    try {
+        await fs.stat(fileName);
+        // Due to the "await" keyword, we *await* the promise to resolve first
+        // myOtherFunction();
+    } catch (err) {
+        /* Important to keep in mind is that errors inside of
+        * the myOtherFunction function also get cought here,
+            which is why you might want to put it outside of
+            the try/catch block. */
+        if (err.code === "ENOENT") await fs.writeFile(fileName, JSON.stringify(data));
+        console.log("FILE CREATED: " + fileName);
+    }
 }
 
-// Happens on startup
-const ReadFile = (fileName) => {
-    fileName = "admindb/" + fileName + ".json";
-    // Read the file
-    fs.readFile(fileName, (err, data) => {
-        if(err) { return console.error(`Read Error ${fileName}`); }
 
-        console.log(`Read ${fileName}`);
-        // Parse the JSON String back into a regular JSON Object
-        // return it so it can be used externally
+const ReadFile = async(fileName) => {
+    // Set the filename path
+    fileName = "admindb/" + fileName + ".json";
+
+    try{
+        let data = await fs.readFile(fileName);
         return JSON.parse(data);
-    });
+    } catch(e){
+        console.error("READ ERROR: " + e);
+        return [{"test": "data"}];
+    }
 }
 
 // Creates or overwrites
-function GenerateFile(fileName, jsonObj){
-    // Parse JSON object to string
-    let data = JSON.stringify(jsonObj);
-
+const GenerateFile = (fileName, jsonObj) => {
+    // Set the filename path
     fileName = "admindb/" + fileName + ".json";
 
     // Create a new file or overwrite existing one
-    fs.writeFile(fileName, data, (err) => {
+    fs.writeFile(fileName, JSON.stringify(jsonObj), (err) => {
         if(err) { return console.error(`Generate Error ${fileName}`); }
 
         console.log(`Generated ${fileName}`);
@@ -50,6 +49,7 @@ function GenerateFile(fileName, jsonObj){
 }
 
 module.exports = {
-    InitFile,
-    GenerateFile
+    GenerateFile,
+    ReadFile,
+    InitFile
 };
