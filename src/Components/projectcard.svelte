fc7f2771831ea4cbe6a8b8d020ed3a6a9f40ae06
ignurof <ignurof@outlook.com>
2021-11-10 11:14:08 +0100

    projectcards have fixed size and use background-image instead for best quality

--- src/Components/projectcard.svelte
+++ src/Components/projectcard.svelte
@@ -24,16 +24,19 @@
         box-shadow: 0px 8px 4px rgba(0, 0, 0, 0.25);
         border-radius: .3em;
         min-width: 33%; /* ONE card = 33% area of parent container aka projects */
-        min-height: 10em;
+        min-height: 18em;
         display: flex;
         flex-direction: column;
     }
 
-    img{
-        width: 100%;
-        height: auto;
-        border-top-left-radius: .3em;
+    .card-image{
         border-top-right-radius: .3em;
+        border-top-left-radius: .3em;
+        min-height: 60%;
+        min-width: 100%;
+        background-position:bottom;
+        background-size: cover;
+        background-repeat: no-repeat;
     }
 
     .card-content{
@@ -68,12 +71,12 @@
         width: 40%;
         margin-left: auto;
         margin-right: 1.4em;
-        margin-top: 1em;
+        margin-top: 2em;
     }
 </style>
 
 <div class="card">
-    <img src={imgSrc} alt="This is a test">
+    <div class="card-image" style="background-image: url({imgSrc});"></div>
     <div class="card-content">
         <h4>{title}</h4>
         <p>{summary}</p>
